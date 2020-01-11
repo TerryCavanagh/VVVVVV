7e491e88d5c530e37cd4142e12057d06eca2018c
Emmanuel Vadot <manu@freebsd.org>
2020-01-11 16:05:24 +0100

    Look for data.zip in the OSDirectory
    For FreeBSD user it mean that they can just pkg install VVVVVV
    once the package will be available and copy the data.zip in
    ~/.local/share/VVVVVV as the executable will be in
    /usr/local/bin/ and users do not have the permission to write files
    in this directory.
    
    Signed-off-by: Emmanuel Vadot <manu@freebsd.org>

--- desktop_version/src/FileSystemUtils.cpp
+++ desktop_version/src/FileSystemUtils.cpp
@@ -73,6 +73,12 @@ int FILESYSTEM_init(char *argvZero)
 #ifdef _WIN32
 	strcpy(output, PHYSFS_getBaseDir());
 	strcat(output, "data.zip");
+#elif defined(__FreeBSD__)
+	PLATFORM_getOSDirectory(output);
+	if (strlcat(output, "data.zip", sizeof(output)) >= sizeof(output)) {
+		puts("Cannot find location for data.zip\n");
+		return 0;
+	}
 #else
 	strcpy(output, "data.zip");
 #endif
