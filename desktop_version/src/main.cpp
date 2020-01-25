2709de9cf0281cf7564f7570e974ba1ccce16b97
Info Teddy <infoteddyatgitgud@cock.li>
2020-01-24 23:30:51 -0800

    Make fast path using FillRect() when opaque
    FillRect() is similar enough to memset when blending isn't used, so the
    game will take a fast path drawing the roomname background when the
    background is opaque.

--- desktop_version/src/main.cpp
+++ desktop_version/src/main.cpp
@@ -127,6 +127,7 @@ int main(int argc, char *argv[])
     SDL_SetSurfaceBlendMode(graphics.backBuffer, SDL_BLENDMODE_NONE);
     graphics.footerbuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, 320, 10, 32, fmt->Rmask, fmt->Gmask, fmt->Bmask, fmt->Amask);
     SDL_SetSurfaceBlendMode(graphics.footerbuffer, SDL_BLENDMODE_BLEND);
+    SDL_SetSurfaceAlphaMod(graphics.footerbuffer, 127);
     FillRect(graphics.footerbuffer, SDL_MapRGB(fmt, 0, 0, 0));
     graphics.Makebfont();
 
