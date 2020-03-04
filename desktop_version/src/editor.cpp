5a25cad74b01c58998f8678332c8fe624099a36c
Misa <infoteddyatgitgud@cock.li>
2020-03-04 12:31:39 -0800

    Don't draw text outline for roomtext in the editor
    Text outline is not drawn on roomtext when you're actually playing the
    game, so don't draw the outline in the editor, either.
    
    FIQ mistakenly added text outline to roomtext in
    ca9f577fc408d417b6bc8b35eac09b757b994964.

--- desktop_version/src/editor.cpp
+++ desktop_version/src/editor.cpp
@@ -2599,7 +2599,7 @@ void editorrender( KeyPoll& key, Graphics& dwgfx, Game& game, mapclass& map, ent
                     int length = utf8::unchecked::distance(edentity[i].scriptname.begin(), edentity[i].scriptname.end());
                     fillboxabs(dwgfx, (edentity[i].x*8)- (ed.levx*40*8),(edentity[i].y*8)- (ed.levy*30*8),length*8,8,dwgfx.getRGB(96,96,96));
                 }
-                dwgfx.bprint((edentity[i].x*8)- (ed.levx*40*8),(edentity[i].y*8)- (ed.levy*30*8), edentity[i].scriptname, 196, 196, 255 - help.glow);
+                dwgfx.Print((edentity[i].x*8)- (ed.levx*40*8),(edentity[i].y*8)- (ed.levy*30*8), edentity[i].scriptname, 196, 196, 255 - help.glow);
                 break;
             case 18: //Terminals
                 dwgfx.drawsprite((edentity[i].x*8)- (ed.levx*40*8),(edentity[i].y*8)- (ed.levy*30*8)+8,17,96,96,96);
