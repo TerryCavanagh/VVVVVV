cc0d5d1d79e0ad59526003f4f8ae044c13f0bb2f
Misa <infoteddyatgitgud@cock.li>
2020-05-17 13:48:07 -0700

    Refactor Labclass.cpp to not use strings for tilemaps
    They now use a const int array instead.

--- desktop_version/src/Labclass.h
+++ desktop_version/src/Labclass.h
@@ -10,7 +10,7 @@
 class labclass
 {
 public:
-    std::vector<std::string>  loadlevel(int rx, int ry);
+    std::vector<int>  loadlevel(int rx, int ry);
 
     std::string roomname;
     int coin, rcol;
