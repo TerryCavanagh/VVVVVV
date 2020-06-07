abbf6bafb93c2e716f93fcf4a561b4c733645538
leo60228 <leo@60228.dev>
2020-06-07 16:11:48 -0400

    Don't leak sounds/music

--- desktop_version/src/Music.cpp
+++ desktop_version/src/Music.cpp
@@ -6,7 +6,13 @@
 
 void musicclass::init()
 {
+	for (size_t i = 0; i < soundTracks.size(); ++i) {
+		Mix_FreeChunk(soundTracks[i].sound);
+	}
 	soundTracks.clear();
+	for (size_t i = 0; i < musicTracks.size(); ++i) {
+		Mix_FreeMusic(musicTracks[i].m_music);
+	}
 	musicTracks.clear();
 
 	musicReadBlob.clear();
