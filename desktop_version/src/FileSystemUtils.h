f4f6206a98dab5517f62330d7c09d1ee91ed856a
Misa <infoteddyatgitgud@cock.li>
2020-06-21 14:26:40 -0700

    Abstract assets mounting to FileSystemUtils.cpp
    The assets mounting code was put directly in editorclass::load(), but
    now it's in a neat little function so it can be called from multiple
    places without having to call editorclass::load().

--- desktop_version/src/FileSystemUtils.h
+++ desktop_version/src/FileSystemUtils.h
@@ -15,6 +15,7 @@ char *FILESYSTEM_getUserLevelDirectory();
 
 bool FILESYSTEM_directoryExists(const char *fname);
 void FILESYSTEM_mount(const char *fname);
+void FILESYSTEM_mountassets(const char *path);
 void FILESYSTEM_unmountassets();
 
 void FILESYSTEM_loadFileToMemory(const char *name, unsigned char **mem,
