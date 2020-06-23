f6d73c62ecb1c1b876b1c98eba9c3e91431a6b35
Misa <infoteddyatgitgud@cock.li>
2020-06-22 21:20:05 -0700

    Allow pressing Esc to go back to pause menu from quit prompt
    Allowing for more actions makes things feel smoother.

--- desktop_version/src/Input.cpp
+++ desktop_version/src/Input.cpp
@@ -1958,13 +1958,20 @@ void mapinput()
         {
             game.press_action = true;
         }
-        if (game.menupage < 9)
+        if (game.menupage < 12)
         {
             if (key.isDown(KEYBOARD_ENTER) || key.isDown(game.controllerButton_map) ) game.press_map = true;
             if (key.isDown(27))
             {
                 game.mapheld = true;
-                game.menupage = 30;
+                if (game.menupage < 9)
+                {
+                    game.menupage = 30;
+                }
+                else
+                {
+                    game.menupage = 31;
+                }
             }
         }
         else
