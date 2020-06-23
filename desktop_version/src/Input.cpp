3c5ed04678e9e253cac93eaea114798f14b61f70
Misa <infoteddyatgitgud@cock.li>
2020-06-22 20:58:09 -0700

    Make "no, keep playing" go back to pause menu
    This is so the menus can stack on top of each other. Well, technically
    they don't, but it's the same thing.

--- desktop_version/src/Input.cpp
+++ desktop_version/src/Input.cpp
@@ -2089,8 +2089,9 @@ void mapmenuactionpress()
         break;
 
     case 10:
-        //return to game
-        graphics.resumegamemode = true;
+        //return to pause menu
+        music.playef(11);
+        game.menupage = 31;
         break;
     case 11:
         //quit to menu
