bf31be6cf350dcc5fc7ef79ed0167c7068e12b41
Misa <infoteddyatgitgud@cock.li>
2020-06-22 22:18:21 -0700

    Set backgrounddrawn to false when returning to pause menu
    This makes it so the tower background doesn't persist and scroll upwards
    if you exit the menu in a warp zone horizontal or vertical room.
    
    Ugh, and while we're on the subject of separating the in-game tower
    background and the menu tower background, could we PLEASE separate the
    horizontal / vertical warp backgrounds from the tower backgrounds, too?!

--- desktop_version/src/Game.cpp
+++ desktop_version/src/Game.cpp
@@ -7763,6 +7763,7 @@ void Game::returntopausemenu()
     gamestate = MAPMODE;
     map.kludge_to_bg();
     map.tdrawback = true;
+    graphics.backgrounddrawn = false;
     game.mapheld = true;
     graphics.flipmode = graphics.setflipmode;
 }
