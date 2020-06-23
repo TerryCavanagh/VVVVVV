809422c9c9eb7e659392c14894fa61f5feb228d5
Misa <infoteddyatgitgud@cock.li>
2020-06-22 22:55:59 -0700

    Allow toggling invincibility/slowdown in intermission replays
    inspecial() includes being in intermission replays, but it's not
    necessary to disable invincibility/slowdown for them.

--- desktop_version/src/Input.cpp
+++ desktop_version/src/Input.cpp
@@ -493,7 +493,7 @@ void menuactionpress()
             break;
         case 3:
             //invincibility
-            if (!game.ingame_titlemode || !game.inspecial())
+            if (!game.ingame_titlemode || (!game.insecretlab && !game.intimetrial && !game.nodeathmode))
             {
                 if (!map.invincibility)
                 {
@@ -514,7 +514,7 @@ void menuactionpress()
             break;
         case 4:
             //change game speed
-            if (!game.inspecial())
+            if (!game.insecretlab && !game.intimetrial && !game.nodeathmode)
             {
                 game.createmenu(Menu::setslowdown);
                 map.nexttowercolour();
