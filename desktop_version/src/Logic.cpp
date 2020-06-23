f01f6a1e343ac5eff9786ec2a51eda7c3c73faa4
Misa <infoteddyatgitgud@cock.li>
2020-06-22 22:46:29 -0700

    Fix deltaframe tower BG flicker when exiting menu in H/V warp room
    To fix this annoying flicker (which, btw, took me WAY too long to do), I
    had to introduce yet another kludge variable to signal that the
    horizontal/vertical warp background should be re-initialized on the
    pause screen.
    
    I think I could technically keep the 'graphics.backgrounddrawn = false;'
    in maplogic() and remove the 'graphics.backgrounddrawn = false;' in
    Game::returntopausemenu(), but I'm keeping that other one around because
    it doesn't hurt and just as a general precaution and safety measure.

--- desktop_version/src/Logic.cpp
+++ desktop_version/src/Logic.cpp
@@ -79,6 +79,16 @@ void maplogic()
     graphics.updatetextboxes();
     graphics.updatetitlecolours();
 
+    if (game.shouldreturntopausemenu)
+    {
+        game.shouldreturntopausemenu = false;
+        graphics.backgrounddrawn = false;
+        if (map.background == 3 || map.background || 4)
+        {
+            graphics.updatebackground(map.background);
+        }
+    }
+
     graphics.crewframedelay--;
     if (graphics.crewframedelay <= 0)
     {
