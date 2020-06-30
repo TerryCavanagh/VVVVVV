141d46195b863c6d6d6e957f343ac266fe0aa4d0
Misa <infoteddyatgitgud@cock.li>
2020-06-30 15:17:02 -0700

    De-duplicate track names
    Instead of copy-pasting the entire list three times over, why not put it
    in an easy to use "X" macro?

--- desktop_version/src/BinaryBlob.h
+++ desktop_version/src/BinaryBlob.h
@@ -4,6 +4,24 @@
 /* Laaaazyyyyyyy -flibit */
 // #define VVV_COMPILEMUSIC
 
+#define TRACK_NAMES \
+	FOREACH_TRACK("data/music/0levelcomplete.ogg") \
+	FOREACH_TRACK("data/music/1pushingonwards.ogg") \
+	FOREACH_TRACK("data/music/2positiveforce.ogg") \
+	FOREACH_TRACK("data/music/3potentialforanything.ogg") \
+	FOREACH_TRACK("data/music/4passionforexploring.ogg") \
+	FOREACH_TRACK("data/music/5intermission.ogg") \
+	FOREACH_TRACK("data/music/6presentingvvvvvv.ogg") \
+	FOREACH_TRACK("data/music/7gamecomplete.ogg") \
+	FOREACH_TRACK("data/music/8predestinedfate.ogg") \
+	FOREACH_TRACK("data/music/9positiveforcereversed.ogg") \
+	FOREACH_TRACK("data/music/10popularpotpourri.ogg") \
+	FOREACH_TRACK("data/music/11pipedream.ogg") \
+	FOREACH_TRACK("data/music/12pressurecooker.ogg") \
+	FOREACH_TRACK("data/music/13pacedenergy.ogg") \
+	FOREACH_TRACK("data/music/14piercingthesky.ogg") \
+	FOREACH_TRACK("data/music/predestinedfatefinallevel.ogg")
+
 struct resourceheader
 {
 	char name[48];
