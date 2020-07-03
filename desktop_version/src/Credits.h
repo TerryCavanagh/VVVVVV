fc03fca8387c21ed1ae45f3ccac84cb68f8242e8
Misa <infoteddyatgitgud@cock.li>
2020-07-03 03:13:15 -0700

    Turn (super)patrons/githubfriends into arrays & move them to new file
    So, originally, I wanted to keep them on Game, but it turns out that if
    I initialize it in Game.cpp, the compiler will complain that other files
    won't know what's actually inside the array. To do that, I'd have to
    initialize it in Game.h. But I don't want to initialize it in Game.h
    because that'd mean recompiling a lot of unnecessary files whenever
    someone gets added to the credits.
    
    So, I moved all the patrons, superpatrons, and GitHub contributors to a
    new file, Credits.h, which only contains the list (and the credits max
    position calculation). That way, whenever someone gets added, only the
    minimal amount of files need to be recompiled.

---
+++ desktop_version/src/Credits.h
@@ -0,0 +1,118 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+#include <SDL.h>
+
+namespace Credits {
+
+/* Terry's Patrons... */
+static const char* superpatrons[] = {
+    "Anders Ekermo",
+    "Andreas K|mper",
+    "Anthony Burch",
+    "Bennett Foddy",
+    "Brendan O'Sullivan",
+    "Christopher Armstrong",
+    "Daniel Benmergui",
+    "David Pittman",
+    "Ian Bogost",
+    "Ian Poma",
+    "Jaz McDougall",
+    "John Faulkenbury",
+    "Jonathan Whiting",
+    "Kyle Pulver",
+    "Markus Persson",
+    "Nathan Ostgard",
+    "Nick Easler",
+    "Stephen Lavelle",
+};
+
+static const char* patrons[] = {
+    "Adam Wendt",
+    "Andreas J{rgensen",
+    "}ngel Louzao Penalva",
+    "Ashley Burton",
+    "Aubrey Hesselgren",
+    "Bradley Rose",
+    "Brendan Urquhart",
+    "Chris Ayotte",
+    "Christopher Zamanillo",
+    "Daniel Schuller",
+    "Hybrid Mind Studios",
+    "Emilie McGinley",
+    "Francisco Solares",
+    "Hal Helms",
+    "Hayden Scott-Baron",
+    "Hermit Games",
+    "Ido Yehieli",
+    "Jade Vault Games",
+    "James Andrews",
+    "James Riley",
+    "James Hsieh",
+    "Jasper Byrne",
+    "Jedediah Baker",
+    "Jens Bergensten",
+    "Jeremy J. Penner",
+    "Jeremy Peterson",
+    "Jim McGinley",
+    "Jonathan Cartwright",
+    "John Nesky",
+    "Jos Yule",
+    "Jose Flores",
+    "Josh Bizeau",
+    "Joshua Buergel",
+    "Joshua Hochner",
+    "Kurt Ostfeld",
+    "Magnus P~lsson",
+    "Mark Neschadimenko",
+    "Matt Antonellis",
+    "Matthew Reppert",
+    "Michael Falkensteiner",
+    "Michael Vendittelli",
+    "Mike Kasprzak",
+    "Mitchel Stein",
+    "Sean Murray",
+    "Simon Michael",
+    "Simon Schmid",
+    "Stephen Maxwell",
+    "Swing Swing Submarine",
+    "Tam Toucan",
+    "Terry Dooher",
+    "Tim W.",
+    "Timothy Bragan",
+};
+
+/* CONTRIBUTORS.txt, again listed alphabetically (according to `sort`) by last name */
+static const char* githubfriends[] = {
+    "Matt \"Fussmatte\" Aaldenberg", // TODO: Change to "Fußmatte" if/when UTF-8 support is added
+    "AlexApps99",
+    "Christoph B{hmwalder",
+    "Charlie Bruce",
+    "Brian Callahan",
+    "Dav999",
+    "Allison Fleischer",
+    "Daniel Lee",
+    "Fredrik Ljungdahl",
+    "Matt Penny",
+    "Elliott Saltar",
+    "Marvin Scholz",
+    "Keith Stellyes",
+    "Elijah Stone",
+    "Thomas S|nger",
+    "Info Teddy",
+    "Alexandra Tildea",
+    "leo60228",
+    "Emmanuel Vadot",
+    "Remi Verschelde", // TODO: Change to "Rémi" if/when UTF-8 support is added
+    "viri",
+    "Wouter",
+};
+
+/* Calculate credits length, finally. */
+static const int creditmaxposition = 1050 + (10 * (
+    SDL_arraysize(superpatrons) + SDL_arraysize(patrons) + SDL_arraysize(githubfriends)
+));
+
+}; /* namespace Credits */
+
+#endif /* CREDITS_H */
