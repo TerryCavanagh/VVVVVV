cb2f72fd8ec1657fe1f0bfc6a0897c31206e7423
Misa <infoteddyatgitgud@cock.li>
2020-09-25 09:08:20 -0700

    Add XMLUtils.cpp and XMLUtils.h
    These XML functions will be useful for de-duplicating copy-pasted XML
    handling code, while also making it so elements will get updated in
    place instead of being thrown out and starting from scratch every time a
    file is saved.

---
+++ desktop_version/src/XMLUtils.h
@@ -0,0 +1,27 @@
+// Forward decl, avoid including tinyxml2.h
+namespace tinyxml2
+{
+    class XMLComment;
+    class XMLDocument;
+    class XMLDeclaration;
+    class XMLElement;
+    class XMLNode;
+}
+
+namespace xml
+{
+
+tinyxml2::XMLElement* update_element(tinyxml2::XMLNode* parent, const char* name);
+// Same thing as above, but takes &parent instead of *parent
+tinyxml2::XMLElement* update_element(tinyxml2::XMLNode& parent, const char* name);
+
+tinyxml2::XMLElement* update_element_delete_contents(tinyxml2::XMLNode* parent, const char* name);
+
+tinyxml2::XMLElement* update_tag(tinyxml2::XMLNode* parent, const char* name, const char* value);
+tinyxml2::XMLElement* update_tag(tinyxml2::XMLNode* parent, const char* name, const int value);
+
+tinyxml2::XMLDeclaration* update_declaration(tinyxml2::XMLDocument& doc);
+
+tinyxml2::XMLComment* update_comment(tinyxml2::XMLNode* parent, const char* text);
+
+} // namespace xml
