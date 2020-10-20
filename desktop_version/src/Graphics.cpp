917d8e6719581071237834d4206bbe5b307c7b23
Misa <infoteddyatgitgud@cock.li>
2020-10-19 23:57:23 -0700

    Make some variables const in drawentities()
    I'm going to refactor drawing an entity out to a separate function, and
    since I'm going to do that, I might as well make some things const to
    clarify intent first and foremost and possibly improve performance or
    compiler optimization.

--- desktop_version/src/Graphics.cpp
+++ desktop_version/src/Graphics.cpp
@@ -1591,26 +1591,18 @@ void Graphics::drawentities()
 
 #if !defined(NO_CUSTOM_LEVELS)
     // Special case for gray Warp Zone tileset!
-    int room = game.roomx-100 + (game.roomy-100) * ed.maxwidth;
-    bool custom_gray = INBOUNDS_ARR(room, ed.level)
+    const int room = game.roomx-100 + (game.roomy-100) * ed.maxwidth;
+    const bool custom_gray = INBOUNDS_ARR(room, ed.level)
     && ed.level[room].tileset == 3 && ed.level[room].tilecol == 6;
 #else
-    bool custom_gray = false;
+    const bool custom_gray = false;
 #endif
 
     std::vector<SDL_Surface*>& tilesvec = (map.custommode && !map.finalmode) ? entcolours : tiles;
 
     std::vector<SDL_Surface*>& spritesvec = flipmode ? flipsprites : sprites;
 
-    int yoff;
-    if (map.towermode)
-    {
-        yoff = lerp(map.oldypos, map.ypos);
-    }
-    else
-    {
-        yoff = 0;
-    }
+    const int yoff = map.towermode ? lerp(map.oldypos, map.ypos) : 0;
 
     for (int i = obj.entities.size() - 1; i >= 0; i--)
     {
@@ -1619,8 +1611,8 @@ void Graphics::drawentities()
             continue;
         }
 
-        int xp = lerp(obj.entities[i].lerpoldxp, obj.entities[i].xp);
-        int yp = lerp(obj.entities[i].lerpoldyp, obj.entities[i].yp);
+        const int xp = lerp(obj.entities[i].lerpoldxp, obj.entities[i].xp);
+        const int yp = lerp(obj.entities[i].lerpoldyp, obj.entities[i].yp);
 
         switch (obj.entities[i].size)
         {
@@ -1669,7 +1661,7 @@ void Graphics::drawentities()
                 wrappedPoint.y -= 230;
             }
 
-            bool isInWrappingAreaOfTower = map.towermode && !map.minitowermode && map.ypos >= 500 && map.ypos <= 5000;
+            const bool isInWrappingAreaOfTower = map.towermode && !map.minitowermode && map.ypos >= 500 && map.ypos <= 5000;
             if (wrapX && (map.warpx || isInWrappingAreaOfTower))
             {
                 drawRect = sprites_rect;
