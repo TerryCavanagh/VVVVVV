7ba15e9787628cdd12a1e412470310f6c1a75101
Misa <infoteddyatgitgud@cock.li>
2020-12-25 20:24:14 -0800

    Update commit hash every time it changes, not just when CMake is re-ran
    The commit hash is now properly updated every time it gets changed, and
    not just when CMake gets re-ran.
    
    For this to work, we need to use a few CMake tricks. We add a custom
    target with ADD_CUSTOM_TARGET(), which is apparently always considered
    out-of-date (but I had to add a BYPRODUCTS line to get it to actually
    work), and we use the target to run a .cmake file every time we build.
    Also, VVVVVV needs to depend on this custom target, to ensure that the
    game gets built AFTER the version gets generated - otherwise there'll be
    an error. So we do an ADD_DEPENDENCIES() after the ADD_EXECUTABLE() for
    VVVVVV.
    
    This file, version.cmake, is just the Version.h.out generation that I
    added previously, but the important thing about all of this is that if
    the contents of Version.h.out doesn't change, and thus if the commit
    hash hasn't changed, then CMake will never recompile and relink anything
    at all. (At least with the Ninja generator.)
    
    On a small note, since the Version.h.out generation is now a separate
    script that is guaranteed to get ran on every single build, while the
    Git FIND_PACKAGE() gets ran only at configure time, it is possible for
    the cached path of the Git executable to get out of date. Fixing this
    requires a simple re-configure (ideally), but in case it wasn't fixed,
    the INTERIM_COMMIT and COMMIT_DATE variables would get set to empty
    strings instead of containing a value. To prevent this from happening,
    I've removed ERROR_QUIET from the EXECUTE_PROCESS() calls in
    version.cmake, because it's better to explicitly error if the Git
    executable wasn't found than implicitly carry on like nothing happened.

---
+++ desktop_version/version.cmake
@@ -0,0 +1,19 @@
+# Expects INPUT_FILE and OUTPUT_FILE to be defined
+
+# Get interim commit and date of commit
+EXECUTE_PROCESS(
+	COMMAND "${GIT_EXECUTABLE}" log -1 --format=%h
+	OUTPUT_VARIABLE INTERIM_COMMIT
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+EXECUTE_PROCESS(
+	COMMAND "${GIT_EXECUTABLE}" log -1 --format=%cs
+	OUTPUT_VARIABLE COMMIT_DATE
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+MESSAGE(STATUS "This is interim commit ${INTERIM_COMMIT} (committed ${COMMIT_DATE})")
+
+# Take the template file and replace the macros with what we have
+CONFIGURE_FILE(${INPUT_FILE} ${OUTPUT_FILE})
