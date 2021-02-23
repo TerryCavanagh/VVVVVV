d9737589dee5b8830775da01d795dd282f831511
Misa <infoteddy@infoteddy.info>
2021-02-23 00:54:45 -0800

    Add logging functions
    Named "vlogs" because they're logs for VVVVVV. Also it's a funny name.

---
+++ desktop_version/src/Vlogging.h
@@ -0,0 +1,19 @@
+#ifndef VLOGGING_H
+#define VLOGGING_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int vlog_info(const char* text, ...);
+
+int vlog_warn(const char* text, ...);
+
+int vlog_error(const char* text, ...);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* VLOGGING_H */
