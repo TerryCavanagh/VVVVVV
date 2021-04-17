c84d7ebf082f5eb0d888ce48829b044ef9d668f9
Misa <infoteddy@infoteddy.info>
2021-04-17 01:14:10 -0700

    Rename vx/vy createentity args to meta1/meta2
    vx/vy mean x-velocity and y-velocity... except here, where it seems like
    they're used as extra parameters that do different things depending on
    the entity. But it seems like at one point they were actually meant to
    be the speed of the entity (this is the case for the unused decorative
    particle entities), and then just never got renamed when they weren't.
    
    The custom levels community named these two parameters meta1 and meta2
    in the reference list of entities for the createentity() script command,
    so that's what I'm naming them here. This will avoid confusion (I know
    that some people reading this function have genuinely mistaken the vx/vy
    for actually meaning x-velocity and y-velocity, simply because they were
    named that way).

--- desktop_version/src/Entity.h
+++ desktop_version/src/Entity.h
@@ -74,14 +74,14 @@ public:
 
     int crewcolour(int t);
 
-    void createentity(int xp, int yp, int t, int vx, int vy,
+    void createentity(int xp, int yp, int t, int meta1, int meta2,
                       int p1, int p2, int p3, int p4);
-    void createentity(int xp, int yp, int t, int vx, int vy,
+    void createentity(int xp, int yp, int t, int meta1, int meta2,
                       int p1, int p2);
-    void createentity(int xp, int yp, int t, int vx, int vy,
+    void createentity(int xp, int yp, int t, int meta1, int meta2,
                       int p1);
-    void createentity(int xp, int yp, int t, int vx, int vy);
-    void createentity(int xp, int yp, int t, int vx);
+    void createentity(int xp, int yp, int t, int meta1, int meta2);
+    void createentity(int xp, int yp, int t, int meta1);
     void createentity(int xp, int yp, int t);
 
     bool updateentities(int i);
