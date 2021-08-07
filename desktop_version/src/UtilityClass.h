ee02aa04991ede6bb2e21da223aaf911f1217b0b
Misa <infoteddy@infoteddy.info>
2021-08-06 23:49:04 -0700

    Add fallthrough macro
    This macro needs to be used because Clang is stupid and doesn't let you
    use /* fallthrough */ comments like GCC does. However, if GCC is too old
    (as is the case on CentOS 7), then it won't recognize __has_attribute
    either.

--- desktop_version/src/UtilityClass.h
+++ desktop_version/src/UtilityClass.h
@@ -68,6 +68,16 @@ void _VVV_between(
         sizeof(middle) \
     )
 
+#ifndef __has_attribute
+#   define __has_attribute(x) 0
+#endif
+
+#if __has_attribute(__fallthrough__)
+#   define VVV_fallthrough __attribute__((__fallthrough__))
+#else
+#   define VVV_fallthrough do { } while (false) /* fallthrough */
+#endif
+
 
 //helperClass
 class UtilityClass
