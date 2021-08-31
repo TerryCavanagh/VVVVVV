62cff254b79b34b41d50d5515631ac26d3d14285
Misa <infoteddy@infoteddy.info>
2021-08-31 10:25:29 -0700

    Bump version to 2.4

--- desktop_version/src/Render.cpp
+++ desktop_version/src/Render.cpp
@@ -159,7 +159,7 @@ static void menurender(void)
 #ifdef INTERIM_COMMIT
         graphics.Print( 310 - (SDL_arraysize(INTERIM_COMMIT) - 1) * 8, 220, INTERIM_COMMIT, tr/2, tg/2, tb/2);
 #endif
-        graphics.Print( 310 - (4*8), 230, "v2.3", tr/2, tg/2, tb/2);
+        graphics.Print( 310 - (4*8), 230, "v2.4", tr/2, tg/2, tb/2);
 
         if(music.mmmmmm){
             graphics.Print( 10, 230, "[MMMMMM Mod Installed]", tr/2, tg/2, tb/2);
