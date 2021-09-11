222e45ada17eec0f77bcfd702e72efb6746d9695
Misa <infoteddy@infoteddy.info>
2021-09-10 19:37:33 -0700

    Music: Do not do fades if not playing
    This fixes a regression where entering playtesting while a track was
    fading out (by exiting out of playtesting with a track playing and then
    immediately entering back in with the level start music set) would
    result in no music.
    
    The cause is the game doing fades even though nothing is playing, which
    puts it in a confusing state.

--- desktop_version/src/Music.cpp
+++ desktop_version/src/Music.cpp
@@ -328,6 +328,11 @@ static enum FadeCode processmusicfade(struct FadeState* state, int* volume)
 
 void musicclass::fadeMusicVolumeIn(int ms)
 {
+	if (halted())
+	{
+		return;
+	}
+
 	m_doFadeInVol = true;
 	m_doFadeOutVol = false;
 
@@ -345,6 +350,11 @@ void musicclass::fadeMusicVolumeIn(int ms)
 
 void musicclass::fadeMusicVolumeOut(const int fadeout_ms)
 {
+	if (halted())
+	{
+		return;
+	}
+
 	m_doFadeInVol = false;
 	m_doFadeOutVol = true;
 
