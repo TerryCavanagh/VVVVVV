bf2f33f1ca388c95e62fb3fa74a980f7f29e28cb
Misa <infoteddy@infoteddy.info>
2021-09-22 19:58:31 -0700

    Add noreturn qualifier to `VVV_exit`
    This function doesn't return, so we mark it as noreturn if the compiler
    supports it.

--- desktop_version/src/Exit.h
+++ desktop_version/src/Exit.h
@@ -1,6 +1,14 @@
 #ifndef VVV_EXIT_H
 #define VVV_EXIT_H
 
-void VVV_exit(const int exit_code);
+#ifdef __GNUC__
+#   define VVV_NORETURN __attribute__((noreturn))
+#elif defined(_MSC_VER)
+#   define VVV_NORETURN __declspec(noreturn)
+#else
+#   define VVV_NORETURN
+#endif
+
+VVV_NORETURN void VVV_exit(const int exit_code);
 
 #endif /* VVV_EXIT_H */
