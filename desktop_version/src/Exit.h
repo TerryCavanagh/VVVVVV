553321501963776c104616cded4d08d1346f7c87
Misa <infoteddy@infoteddy.info>
2021-09-27 10:32:23 -0700

    Use `SDL_NORETURN`
    I misread SDL's code and thought that SDL's `begin_code.h` was internal
    only to SDL. It turns out you get it when you include basically any
    header, such as `SDL_stdinc.h`. So use it directly instead of copying it
    for our own.

--- desktop_version/src/Exit.h
+++ desktop_version/src/Exit.h
@@ -1,14 +1,8 @@
 #ifndef VVV_EXIT_H
 #define VVV_EXIT_H
 
-#ifdef __GNUC__
-#   define VVV_NORETURN __attribute__((noreturn))
-#elif defined(_MSC_VER)
-#   define VVV_NORETURN __declspec(noreturn)
-#else
-#   define VVV_NORETURN
-#endif
+#include <SDL_stdinc.h>
 
-VVV_NORETURN void VVV_exit(const int exit_code);
+SDL_NORETURN void VVV_exit(const int exit_code);
 
 #endif /* VVV_EXIT_H */
