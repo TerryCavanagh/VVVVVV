dfb1e31d781ccd84cf009be12839cc17ba59b1b2
Misa <infoteddy@infoteddy.info>
2021-12-25 23:54:43 -0800

    Optimization: Don't outline if room name BG opaque
    This is quite simple. Just use a function pointer that switches out
    which function we're going to use.
    
    ...Or not. C++ syntax makes this a bit awful since the function is a
    member of a class. Did I mention how much I don't like C++?

--- desktop_version/src/Render.cpp
+++ desktop_version/src/Render.cpp
@@ -1733,15 +1733,18 @@ void gamerender(void)
     if(map.extrarow==0 || (map.custommode && map.roomname[0] != '\0'))
     {
         const char* roomname;
+        void (Graphics::*printfunc)(int, int, const std::string&, int, int, int, bool);
 
         graphics.footerrect.y = 230;
         if (graphics.translucentroomname)
         {
             SDL_BlitSurface(graphics.footerbuffer, NULL, graphics.backBuffer, &graphics.footerrect);
+            printfunc = &Graphics::bprint;
         }
         else
         {
             FillRect(graphics.backBuffer, graphics.footerrect, 0);
+            printfunc = &Graphics::Print;
         }
 
         if (map.finalmode)
@@ -1753,7 +1756,7 @@ void gamerender(void)
             roomname = map.roomname;
         }
 
-        graphics.bprint(5, 231, map.roomname, 196, 196, 255 - help.glow, true);
+        (graphics.*printfunc)(5, 231, roomname, 196, 196, 255 - help.glow, true);
     }
 
     if (map.roomtexton)
