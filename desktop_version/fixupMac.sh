8980b2e546f6342cbcbd85016ac7f7f378160d7b
Ethan Lee <flibitijibibo@gmail.com>
2022-03-29 02:27:15 -0400

    Remove SDL2_mixer line from fixupMac.sh

--- desktop_version/fixupMac.sh
+++ desktop_version/fixupMac.sh
@@ -3,6 +3,5 @@
 # flibit has this solely for convenience when cleaning up Mac binaries.
 
 install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @rpath/libSDL2-2.0.0.dylib VVVVVV 
-install_name_tool -change /usr/local/lib/libSDL2_mixer-2.0.0.dylib @rpath/libSDL2_mixer-2.0.0.dylib VVVVVV 
 strip -S VVVVVV
 otool -L VVVVVV
