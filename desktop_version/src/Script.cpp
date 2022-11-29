63b8c712649a5e486c7fe60aa0c0d635568a3ef8
fraZ0R <30442287+f-raZ0R@users.noreply.github.com>
2022-11-29 18:04:46 -0500

    Actually fix #913 oops

--- desktop_version/src/Script.cpp
+++ desktop_version/src/Script.cpp
@@ -3543,7 +3543,7 @@ void scriptclass::loadcustom(const std::string& t)
                 break;
             }
             int ti=help.Int(words[1].c_str());
-            int nti = ti>=0 && ti<=50 ? ti : 1;
+            int nti = ti>=0 ? ti : 1;
             for(int ti2=0; ti2<nti; ti2++){
                 i++;
                 if(INBOUNDS_VEC(i, lines)){
@@ -3568,7 +3568,7 @@ void scriptclass::loadcustom(const std::string& t)
             add("text(cyan,0,0,"+words[1]+")");
 
             int ti=help.Int(words[1].c_str());
-            int nti = ti>=0 && ti<=50 ? ti : 1;
+            int nti = ti>=0 ? ti : 1;
             for(int ti2=0; ti2<nti; ti2++){
                 i++;
                 if(INBOUNDS_VEC(i, lines)){
