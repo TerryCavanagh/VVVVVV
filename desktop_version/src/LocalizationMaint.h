437c2d56219f4f9d87a78b27f8c76c5640de6b36
Dav999-v <dav999.tolp@gmail.com>
2022-12-29 05:23:41 +0100

    Add LocalizationMaint.cpp/h (not compiled yet)
    This commit is part of rewritten history of the localization branch.
    The original (unsquashed) commit history can be found here:
    https://github.com/Dav999-v/VVVVVV/tree/localization-orig

---
+++ desktop_version/src/LocalizationMaint.h
@@ -0,0 +1,17 @@
+#ifndef LOCALIZATIONMAINT_H
+#define LOCALIZATIONMAINT_H
+
+namespace loc
+{
+
+bool sync_lang_files(void);
+
+bool save_roomname_to_file(const std::string& langcode, bool custom_level, int roomx, int roomy, const char* tra, const char* explanation);
+bool save_roomname_explanation_to_files(bool custom_level, int roomx, int roomy, const char* explanation);
+
+void local_limits_check(void);
+void global_limits_check(void);
+
+} /* namespace loc */
+
+#endif /* LOCALIZATIONMAINT_H */
