ddaabb3efe7aa9c700cd222965a4018969645f29
Dav999-v <dav999.tolp@gmail.com>
2023-01-17 22:18:39 +0100

    Completely remove Graphics::bprint
    Only three deprecated functions remain:
    - Graphics::Print
    - Graphics::PrintWrap
    - Graphics::bigprint
    
    I also fixed multiline transparent textboxes having their outlines
    overlap the text itself, and fixed textboxclass::padtowidth assuming
    glyph widths of 8 (it made the hints at the start of intermission 1
    run offscreen for example)

--- desktop_version/src/Graphics.h
+++ desktop_version/src/Graphics.h
@@ -206,8 +206,6 @@ public:
 
     int PrintWrap(int x, int y, const std::string& s, int r, int g, int b, bool cen = false, int linespacing = -1, int maxwidth = -1);
 
-    void bprint(int x, int y, const std::string& t, int r, int g, int b, bool cen = false);
-
     void bigprint( int _x, int _y, const std::string& _s, int r, int g, int b, bool cen = false, int sc = 2 );
     void drawspritesetcol(int x, int y, int t, int c);
 
