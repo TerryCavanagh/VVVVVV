TerryCavanagh/VVVVVV

commit 88142ea8395266ebaec508e9fde3dc76b0f67bc0

Fix legend positions

desktop_version/src/Render.cpp
@@ -2060,20 +2060,23 @@ static MapRenderData getmaprenderdata()
     data.zoom = map.custommode ? map.customzoom : 1;
     data.xoff = map.custommode ? map.custommmxoff : 0;
     data.yoff = map.custommode ? map.custommmyoff : 0;
+    data.legendxoff = 40 + data.xoff;
+    data.legendyoff = 21 + data.yoff;
 
+    // Magic numbers for centering legend tiles.
     switch (data.zoom)
     {
     case 4:
-        data.legendxoff = 60;
-        data.legendyoff = 35;
+        data.legendxoff += 21;
+        data.legendyoff += 16;
         break;
     case 2:
-        data.legendxoff = 48;
-        data.legendyoff = 26;
+        data.legendxoff += 9;
+        data.legendyoff += 5;
         break;
     default:
-        data.legendxoff = 43;
-        data.legendyoff = 22;
+        data.legendxoff += 3;
+        data.legendyoff += 1;
         break;
     }
 

commit 556e3a110aec8c4e6a157296898b78e68d7bfe62

Fix small bug with map being off in custom levels
The pixel border around the map fits to map size normally. However, when
the map is off, it's always the dimensions of the full size map, and the
border didn't reflect that, so if the custom minimap was off, and the
map wasn't the full size, it wouldn't fit correctly.

This bug was introduced in PR #898.

desktop_version/src/Render.cpp
@@ -2277,7 +2277,7 @@ static MapRenderData getmaprenderdata()
 static void rendermap(void)
 {
 #ifndef NO_CUSTOM_LEVELS
-    if (map.custommode)
+    if (map.custommode && map.customshowmm)
     {
         graphics.drawpixeltextbox(35 + map.custommmxoff, 16 + map.custommmyoff, map.custommmxsize + 10, map.custommmysize + 10, 65, 185, 207);
         graphics.drawpartimage(graphics.minimap_mounted ? 1 : 12, 40 + map.custommmxoff, 21 + map.custommmyoff, map.custommmxsize, map.custommmysize);