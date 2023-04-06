TerryCavanagh/VVVVVV

commit 1de459d9b414180d5049cbf153266a8d6e00c227

Fix nice fade variables persisting when they shouldn't
This fixes a regression from 2.3 where the very beginning of A New
Dimension isn't silent.

A New Dimension's level music is set to Predestined Fate, but there is a
script box the player touches right upon spawning that stops playing
music. Then after the player ascends upwards, they touch a script box
that plays Predestined Fate. But in 2.2 and before, the very beginning
is silent due to the script box that stops music.

However in 2.3, due to the changes made to playing music during a fade,
the initial level music trying to play Predestined Fate during a music
fadeout from the main menu resulted in Predestined Fate being stored in
the nice fade variables, which kicked in after halting the music since
halting didn't reset those variables.

This resets those variables whenever music is halted, and now the
beginning of A New Dimension is back to how it was in 2.2 and before.

desktop_version/src/Music.cpp
@@ -1032,6 +1032,8 @@ void musicclass::haltdasmusik(void)
     currentsong = -1;
     m_doFadeInVol = false;
     m_doFadeOutVol = false;
+    nicefade = false;
+    nicechange = -1;
 }
 
 void musicclass::silencedasmusik(void)

commit 5fbdf2b66395cf32454addbb73ddb7265f0f0e52

Fix regression with music fade transitions not working
Unfortunately, 1de459d9b414180d5049cbf153266a8d6e00c227 caused a
regression where musicclass::niceplay() didn't work, because fading out
the music would cause haltdasmusik() to be called, which would reset the
fade variables.

To fix this, haltdasmusik() will now only reset the fade variables if
it's not called from a fade, which is signaled with a function
parameter.

desktop_version/src/Music.cpp
@@ -1027,13 +1027,21 @@ void musicclass::pause(void)
 
 void musicclass::haltdasmusik(void)
 {
+    haltdasmusik(false);
+}
+
+void musicclass::haltdasmusik(const bool from_fade)
+{
     /* Just pauses music. This is intended. */
     pause();
     currentsong = -1;
     m_doFadeInVol = false;
     m_doFadeOutVol = false;
-    nicefade = false;
-    nicechange = -1;
+    if (!from_fade)
+    {
+        nicefade = false;
+        nicechange = -1;
+    }
 }
 
 void musicclass::silencedasmusik(void)
@@ -1145,7 +1153,7 @@ void musicclass::processmusicfadeout(void)
     {
         musicVolume = 0;
         m_doFadeOutVol = false;
-        haltdasmusik();
+        haltdasmusik(true);
     }
 }
 

desktop_version/src/Music.h
@@ -23,6 +23,7 @@ public:
     void resumefade(const int fadein_ms);
     void pause(void);
     void haltdasmusik(void);
+    void haltdasmusik(bool from_fade);
     void silencedasmusik(void);
     void fadeMusicVolumeIn(int ms);
     void fadeMusicVolumeOut(const int fadeout_ms);