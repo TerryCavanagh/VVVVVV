c913b49a57e5453087e1b94043d3cbdd4be393e0
Misa <infoteddy@infoteddy.info>
2024-01-09 11:30:17 -0800

    Hook Steam screenshots and send internal capture
    Using the Steamworks API, we can hook the screenshot function and listen
    for a screenshot request callback to send in our own screenshot. This
    applies the screenshot improvements to Steam screenshots as well.
    
    Doing this requires adding some C wrapper functions, as our interface
    with the Steam API is only conducted through C.

--- desktop_version/src/CWrappers.h
+++ desktop_version/src/CWrappers.h
@@ -1,6 +1,7 @@
 #ifndef CWRAPPERS_H
 #define CWRAPPERS_H
 
+#include <SDL_surface.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -9,6 +10,8 @@ extern "C" {
 
 char* HELP_number_words(int _t, const char* number_class);
 uint32_t LOC_toupper_ch(uint32_t ch);
+SDL_Surface* GRAPHICS_tempScreenshot(void);
+uint8_t UTIL_TakeScreenshot(SDL_Surface** surface);
 
 #ifdef __cplusplus
 }
