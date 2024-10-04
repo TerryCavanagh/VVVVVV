c5a70f981be91c33c8fd07e8b92a609019cf768f
NyakoFox <nyakowofox@gmail.com>
2024-10-03 22:12:36 -0300

    Cancelling placing a terminal/script box shouldn't have undo events

--- desktop_version/src/Editor.cpp
+++ desktop_version/src/Editor.cpp
@@ -2377,6 +2377,13 @@ static void commit_roomdata_tiles_change()
     ed.redo_buffer.clear();
 }
 
+static void uncommit()
+{
+    extern editorclass ed;
+
+    ed.undo_buffer.pop_back();
+}
+
 static void set_tile_interpolated(const int x1, const int x2, const int y1, const int y2, const int tile)
 {
     extern editorclass ed;
@@ -3848,6 +3855,9 @@ void editorinput(void)
                     if (ed.old_entity_text == "")
                     {
                         ed.remove_entity(ed.text_entity);
+                        // Uncommit the past two actions
+                        uncommit();
+                        uncommit();
                     }
                 }
 
